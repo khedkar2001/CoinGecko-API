326c76d153c909edd9112e2c04732294223b1a09
khedkar2001 <saurabhrkhedkar849@gmail.com>
2025-08-31 00:34:39 +0530

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Data from './components/Data'
+import { useEffect, useState } from "react";
+
+import "./App.css";
+import axios from "axios";
+import Table from "./components/Table";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      });
+    // console.log('response: ', response);
+  }, []);
 
   return (
-    <>
-      <Data/>
-    </>
-  )
+    <div className="main">
+      <h1 className="header">Coin Geko</h1>
+      {<Table data={data} />}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React from "react";
 
-const Table = (props) => {
+const Table = ({ data }) => {
   return (
-    <div>
-       <table class="table table-bordered">
+   <div className="table-container"> 
+      <table>
         <thead>
-            <tr>
-                <th>name</th>
-                <th>id</th>
-                <th>symbol</th>
-                <th>current_price</th>
-                <th>total_volume</th>
-            </tr>
+          <tr>
+            <th>Icon</th>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Id</th>
+            <th>Current Price</th>
+            <th>All Time High</th>
+            <th>Market Cap</th>
+            <th>Total Volume</th>
+            <th>Price Change % 24 hour</th>
+            <th>Total Supply</th>
+            <th>Max Supply</th>
+          </tr>
         </thead>
         <tbody>
-
-          {props.data.map((item ,index)=>(
-            <tr key={index}>
-             <td>{item.name}</td>
-             <td>{item.id}</td>
-             <td>{item.symbol}</td>
-             <td>{item.current_price}</td>
-             <td>{item.total_volume}</td>
-            </tr>
-          ))} 
+          {data.map((coin, index) => {
+            return (
+              <tr key={index}>
+                <td><img src={coin.image}></img></td>
+                <td>{coin.symbol}</td>
+                <td>{coin.name}</td>
+                <td>{coin.id}</td>
+                <td>{coin.current_price}</td>
+                <td>{coin.ath}</td>
+                <td>{coin.market_cap}</td>
+                <td>{coin.total_volume}</td>
+                <td>{coin.price_change_percentage_24h}</td>
+                <td>{coin.total_supply}</td>
+                <td>{coin.max_supply}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
